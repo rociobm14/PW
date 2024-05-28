26fe3c35f51e85f572821eef863ed55ab3b57227
rorris <rociobarragan14@gmail.com>
2024-05-28 21:30:56 +0200

    show collection dinamic by filters

--- pe2/coleccion.php
+++ pe2/coleccion.php
@@ -25,11 +25,61 @@
                     echo "<p>Conexión fallida: " . $e->getMessage() . "</p>";
                 }
 
-                $consultaSQL = "SELECT * FROM Imagenes WHERE id BETWEEN 1 AND 9";
-                $st = $conexion->prepare($consultaSQL);
-                $st->execute();
+                $numero_obras = 9;
 
-                $imagenes = $st->fetchAll(PDO::FETCH_ASSOC);
+                //calculamos en que pagina estamos
+                if(!isset($_GET['pagina'])){
+                    $pagina = 1;
+                }
+
+                else
+                    $pagina = $_GET['pagina'];
+
+
+                //calculamos la imagen desde la que se empieza
+                $inicio = ($pagina * $numero_obras) - $numero_obras;
+
+                // si se nos ha pasado un autor, añadirlo a la consulta
+                if (isset($_GET['autor'])){
+                    $consultaSQL = "SELECT * FROM Imagenes WHERE autor = :autor LIMIT $inicio, $numero_obras";
+
+                }
+
+                else if (isset($_GET['tema'])){
+                    $consultaSQL = "SELECT * FROM Imagenes WHERE tema = :tema LIMIT $inicio, $numero_obras";
+
+                }
+
+                else if (isset($_GET['epoca'])){
+                    $consultaSQL = "SELECT * FROM Imagenes WHERE epoca = :epoca LIMIT $inicio, $numero_obras";
+
+                }
+
+                else{
+                    $consultaSQL = "SELECT * FROM Imagenes LIMIT $inicio, $numero_obras";
+                }
+
+            
+                $stmt = $conexion->prepare($consultaSQL);
+
+                if(isset($_GET['autor'])){
+                    $autor = $_GET['autor'];
+                    $stmt->bindValue(':autor', $autor);
+                }
+
+                if(isset($_GET['tema'])){
+                    $tema = $_GET['tema'];
+                    $stmt->bindValue(':tema', $tema);
+                }
+
+                if(isset($_GET['epoca'])){
+                    $epoca = $_GET['epoca'];
+                    $stmt->bindValue(':epoca', $epoca);
+                }
+
+                $stmt->execute();
+
+                $imagenes = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
                 if($imagenes){
                     echo '<section id="obras">';
@@ -39,7 +89,7 @@
                     foreach ($imagenes as $imagen){
                         echo '<article>';
                         echo '<figure>';
-                        echo '<a href="obra1.php">';
+                        echo '<a href="obra.php?id=26">';
                         echo '<img src="imagenes/' . $imagen['nombre'] . '" alt="obrasgeneral">';
                         echo '</a>';
                         echo '<figcaption>';
@@ -50,7 +100,7 @@
                         echo '</figure>';
 
                         if(isset($_SESSION['usuario']) && $_SESSION['usuario']['tipo_usuario'] == 'administrador'){
-                            echo '<a class="modificar" href="modificar_obra.php?id=' . $imagen['id'] . '">Modificar</a>';
+                            echo '<a class="modificar" href="modificar_obra.php?id=' . $imagen['id'] . '&nombre=' . $imagen['titulo'] . '">Modificar</a>';
                             echo '<a class="eliminar" href="eliminar_obra.php?id=' . $imagen['id'] . '">Eliminar</a>';
                         }
                        
@@ -58,13 +108,48 @@
                     }
 
                     echo '</section>';
-                    echo '<section id="siguientes">';
-                    echo '<nav>';
-                    echo '<a href="siguientes_generales.php">Siguiente</a>';
-                    echo '</nav>';
-                    echo '</section>';
                 }
 
+                echo '<section id="siguientes">';
+                echo '<nav>';
+
+                if(isset($_GET['autor'])){
+
+                    if($pagina > 1){ //Si no estamos en la primera página mostramos el enlace de anterior
+                        echo '<a href="coleccion.php?pagina=' . ($pagina - 1) . '&autor=' . $_GET['autor'] . '">Anterior</a>';
+                    }
+                    echo '<a href="coleccion.php?pagina=' . ($pagina + 1) . '&autor=' . $_GET['autor'] . '">Siguiente</a>';
+
+                }
+
+                else if(isset($_GET['tema'])){
+
+                    if($pagina > 1){ //Si no estamos en la primera página mostramos el enlace de anterior
+                        echo '<a href="coleccion.php?pagina=' . ($pagina - 1) . '&tema=' . $_GET['tema'] . '">Anterior</a>';
+                    }
+                    echo '<a href="coleccion.php?pagina=' . ($pagina + 1) . '&tema=' . $_GET['tema'] . '">Siguiente</a>';
+
+                }
+
+                else if(isset($_GET['epoca'])){
+
+                    if($pagina > 1){ //Si no estamos en la primera página mostramos el enlace de anterior
+                        echo '<a href="coleccion.php?pagina=' . ($pagina - 1) . '&epoca=' . $_GET['epoca'] . '">Anterior</a>';
+                    }
+                    echo '<a href="coleccion.php?pagina=' . ($pagina + 1) . '&epoca=' . $_GET['epoca'] . '">Siguiente</a>';
+
+                }
+
+                else{
+
+                    if($pagina > 1){ //Si no estamos en la primera página mostramos el enlace de anterioe
+                        echo '<a href="coleccion.php?pagina=' . ($pagina - 1) . '">Anterior</a>';
+                    }
+                    echo '<a href="coleccion.php?pagina=' . ($pagina + 1) . '">Siguiente</a>';
+                }
+
+                echo '</nav>';
+                echo '</section>';
             ?>
         </main>
 
