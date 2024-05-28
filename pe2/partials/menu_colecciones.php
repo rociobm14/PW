d3900462cad5fd2c6b716f5201af7b1154327e52
rorris <rociobarragan14@gmail.com>
2024-05-28 21:31:45 +0200

    dynamic menu for filtering

--- pe2/partials/menu_colecciones.php
+++ pe2/partials/menu_colecciones.php
@@ -7,27 +7,46 @@
                 echo '<a href="aniadir_obra.php">Añadir Obra</a>';
                 echo '</section>';
             }
-       
-       ?>
-        <section>
-            <h3>Autores</h3>
-            <a href="coleccion_mariabe.php">María BE</a>
-            <a href="coleccion_evabe.php">Eva BE</a>
-            <a href="coleccion_noahbe.php">Noah BE</a>
-        </section>
 
-        <section>
-            <h3>Temas</h3>
-            <a href="coleccion_paisajes.php">Paisajes</a>
-            <a href="coleccion_mujeres.php">Mujeres</a>
-            <a href="coleccion_animales.php">Animales</a>
-        </section>
+            //Conexión a la BBDD
+            $dsn = "mysql:host=localhost;dbname=dbrociobarragan_pw2324";
+            $usuario= "pwrociobarragan";
+            $password= "23rociobarragan24";
+            try {
+                $conexion = new PDO( $dsn, $usuario, $password );
+                $conexion->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+            } catch ( PDOException $e ) {
+                echo "<p>Conexión fallida: " . $e->getMessage() . "</p>";
+            }
 
-        <section>
-            <h3>Épocas</h3>
-            <a href="coleccion_otono.php">Otoño</a>
-            <a href="coleccion_primavera.php">Primavera</a>
-            <a href="coleccion_verano.php">Verano</a>
-        </section> 
+            //sacamos los campos existentes de  autores, tema y epocas de la tabla de Imagenes
+            // Consultas para obtener los valores únicos de autores, temas y epocas
+            $autores = $conexion->query("SELECT DISTINCT autor FROM Imagenes")->fetchAll(PDO::FETCH_COLUMN);
+            $temas = $conexion->query("SELECT DISTINCT tema FROM Imagenes")->fetchAll(PDO::FETCH_COLUMN);
+            $epocas = $conexion->query("SELECT DISTINCT epoca FROM Imagenes")->fetchAll(PDO::FETCH_COLUMN);
+
+           
+            echo '<section>';
+            echo '<h3>Autores</h3>';
+            foreach ($autores as $autor){
+                echo '<a href="coleccion.php?autor=' . urlencode($autor) . '">' . htmlentities($autor) . '</a>';
+            }
+            echo '</section>';
+
+            echo '<section>';
+            echo '<h3>Temas</h3>';
+            foreach ($temas as $tema){
+                echo '<a href="coleccion.php?tema=' . urlencode($tema) . '">' . htmlentities($tema) . '</a>';
+            }
+            echo '</section>';
+
+            echo '<section>';
+            echo '<h3>Épocas</h3>';
+            foreach ($epocas as $epoca){
+                echo '<a href="coleccion.php?epoca=' . urlencode($epoca) . '">' . htmlentities($epoca) . '</a>';
+            }
+            echo '</section>';
+        ?>
+
     </nav>
 </aside>
